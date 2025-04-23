ea0aa7671b0d8c950cd96d59c4456facadfd7733
unknown <durgeshsingh12712@gmail.com>
2025-04-23 17:25:35 +0530

    Model Trainer Added

--- bookrecommondedsystem/entity/config_entity.py
+++ bookrecommondedsystem/entity/config_entity.py
@@ -18,3 +18,9 @@ DataTransformationConfig = namedtuple("DataTransformationConfig", [
     "clean_data_file_path",
     "transformed_data_dir"
 ])
+
+ModelTrainerConfig = namedtuple("ModelTrainerConfig", [
+    "transformed_data_file_dir",
+    "trained_model_dir",
+    "trained_model_name"
+])
